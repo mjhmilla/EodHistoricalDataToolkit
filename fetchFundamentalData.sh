00bfdc0fd5dff76b178fae8a1cccda8d205ff775
mjhmilla <millard.matthew@gmail.com>
2023-07-25 22:36:22 +0200

    small update to the fetch scripts: forgot to include the exchange name

--- fetchFundamentalData.sh
+++ fetchFundamentalData.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd build
-./fetch -f ${EOD_TOOLKIT_HOME}/data/fundamentalData/ -u ${EOD_FUNDAMENTAL_DATA} -k ${EOD_API_TOKEN} -t ${EOD_TOOLKIT_HOME}/data/STU.json
+./fetch -f ${EOD_TOOLKIT_HOME}/data/fundamentalData/ -u ${EOD_FUNDAMENTAL_DATA} -k ${EOD_API_TOKEN} -t ${EOD_TOOLKIT_HOME}/data/STU.json -x STU -v
 cd ..
--- fetchHistoricalData.sh
+++ fetchHistoricalData.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd build
-./fetch -f ${EOD_TOOLKIT_HOME}/data/historicalData/ -u ${EOD_HISTORICAL_DATA} -x STU -k ${EOD_API_TOKEN} -t ${EOD_TOOLKIT_HOME}/data/STU.json
+./fetch -f ${EOD_TOOLKIT_HOME}/data/historicalData/ -u ${EOD_HISTORICAL_DATA} -x STU -k ${EOD_API_TOKEN} -t ${EOD_TOOLKIT_HOME}/data/STU.json -v
 cd ..
