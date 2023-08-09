57db163b7ccf4ac651d6cdcaf9a35fd214cd9ae6
mjhmilla <millard.matthew@gmail.com>
2023-08-09 22:22:53 +0200

    Added a method to score how similar two company names are aand to accept a patch based on the total number of characters from exactly matching words from within the name. It works pretty well.

--- src/generatePatch.cc
+++ src/generatePatch.cc
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <fstream>
 #include <string>
+#include <regex>
 
 #include <nlohmann/json.hpp>
 #include <tclap/CmdLine.h>
@@ -9,6 +10,10 @@
 #include <filesystem>
 
 #include "FinancialAnalysisToolkit.h"
+
+#include <boost/algorithm/string/classification.hpp> // Include boost::for is_any_of
+#include <boost/algorithm/string/split.hpp> // Include for boost::split
+#include <boost/algorithm/string.hpp> // Include for boost::iequals
 
 unsigned int COLUMN_WIDTH = 30;
 
@@ -18,6 +23,84 @@ struct primaryTickerPatchData {
   std::vector< std::string > tickersInError;
   std::vector< bool > patchFound;
 };
+
+
+unsigned int calcLengthOfMatchingWords(std::string& strA, std::string& strB){
+  std::vector<std::string> wordsA,wordsB;
+  boost::split(wordsA, strA, boost::is_any_of(" "), boost::token_compress_on);
+  boost::split(wordsB, strB, boost::is_any_of(" "), boost::token_compress_on);
+
+  unsigned int lengthOfMatchingWords=0;
+  for(unsigned int i=0; i<wordsA.size();++i){
+    for(unsigned int j=0; j<wordsB.size();++j){
+      if(boost::iequals(wordsA[i],wordsB[j])){
+        lengthOfMatchingWords += wordsA[i].length();
+      }
+    }
+  }
+
+  return lengthOfMatchingWords;
+}
+
+void trim(std::string& str,
+          const std::string& whitespace)
+{
+    const auto strBegin = str.find_first_not_of(whitespace);
+    if (strBegin != std::string::npos){
+      const auto strEnd = str.find_last_not_of(whitespace);
+      const auto strRange = strEnd - strBegin + 1;
+      str = str.substr(strBegin, strRange);
+    }
+
+}
+// Function to find Levenshtein distance between string `X` and `Y`.
+// `m` and `n` is the total number of characters in `X` and `Y`, respectively
+// This source code comes from:
+//    https://www.techiedelight.com/levenshtein-distance-edit-distance-problem/
+// which was described in
+//    https://en.wikipedia.org/wiki/Levenshtein_distance
+unsigned int calcLevenshteinDistance(std::string& x, std::string& y)
+{
+    unsigned int m = x.length();
+    unsigned int n = y.length();  
+
+    // For all pairs of `i` and `j`, `T[i, j]` will hold the Levenshtein distance
+    // between the first `i` characters of `X` and the first `j` characters of `Y`.
+    // Note that `T` holds `(m+1)×(n+1)` values.
+    int T[m + 1][n + 1];
+ 
+    // initialize `T` by all 0's
+    std::memset(T, 0, sizeof(T));
+ 
+    // we can transform source prefixes into an empty string by
+    // dropping all characters 
+    for (int i = 1; i <= m; ++i){
+        T[i][0] = i;                // (case 1)
+    }
+ 
+    // we can reach target prefixes from empty source prefix
+    // by inserting every character 
+    for (int j = 1; j <= n; ++j){
+        T[0][j] = j;                // (case 1)
+    }
+ 
+    int substitutionCost;
+ 
+    // fill the lookup table in a bottom-up manner
+    for (int i = 1; i <= m; ++i){
+        for (int j = 1; j <= n; ++j){
+            if (x[i - 1] == y[j - 1]) {                 // (case 2)
+                substitutionCost = 0;                   // (case 2)
+            } else{
+                substitutionCost = 1;                   // (case 3c)
+            }
+            T[i][j] = std::min(std::min(T[i - 1][j] + 1,      // deletion (case 3b)
+                                        T[i][j - 1] + 1),     // insertion (case 3a)
+                      T[i - 1][j - 1] + substitutionCost);    // replace (case 2 & 3c)
+        }
+    }
+    return T[m][n];
+}
 
 int main (int argc, char* argv[]) {
 
@@ -170,7 +253,7 @@ int main (int argc, char* argv[]) {
               {"PatchFound",false},
               {"PatchPrimaryTicker", ""},
               {"PatchName",""},
-              {"PatchFitScore",0.}
+              {"PatchSimilarityScore",0.}
             }
           );
 
@@ -234,43 +317,94 @@ int main (int argc, char* argv[]) {
       for(unsigned int indexTicker=0; 
             indexTicker<itPatchData.tickersInError.size();  ++indexTicker){
 
-        bool nameFound = false;
+
         std::string companyNameInError;
         std::string tickerName = itPatchData.tickersInError[indexTicker];
         FinancialAnalysisToolkit::
           getJsonString(scanResults[tickerName]["Name"],companyNameInError);
+
+        //Remove text between pairs of ( ) and - -
+        companyNameInError = std::regex_replace(companyNameInError, 
+                                                std::regex("-.*-.*\\(.*\\)"), "");
+        //Remove non-alphanumeric characters                                                
+        companyNameInError = std::regex_replace(companyNameInError, 
+                                                std::regex("[^a-zA-Z\\d\\s:]"), "");
+        //Trim leading and following whitespace
+        trim(companyNameInError," ");
+
+        unsigned int minScoreA = std::round(companyNameInError.length()*0.5); //Just to filter out Tbk, GmbH, etc
+        if(minScoreA < 5){
+          minScoreA = 5;
+        }
+
+        bool candidateFound = false;
+        unsigned int score  = 0;
+        double bestScore    = patchResults[tickerName]["PatchSimilarityScore"];
+
+        std::string bestName;
+        std::string bestCode;
+
         std::string companyName;
 
         for(auto& itSym : exchangeSymbolList){
           FinancialAnalysisToolkit::getJsonString(itSym["Name"],companyName);
+          
+          //Remove text between pairs of ( ) and - -
+          //companyName = std::regex_replace(companyName, 
+          //                                std::regex("-.*-.*\\(.*\\)"), "");
+          //Remove non-alphanumeric characters                                                                          
+          //companyName = std::regex_replace(companyName, 
+          //                               std::regex("[^a-zA-Z\\d\\s:]"), "");
+          //Trim leading and following whitespace
+          //trim(companyNameInError," ");
 
-          if(companyNameInError.compare(companyName)==0){
-            nameFound=true;
-            std::string eodCode;
+
+          //The LevenshteinDistance is really neat, but it fails when
+          //the word that would match best has additional stuff added to it
+          //that is longer. In this case a company that doesn't share any
+          //exact matching words but has the same length can have the 
+          //lowest Levenshtein distance.
+          //
+          //What I really want to know is how many words in each string
+          //exactly match, and to calculate those matching characters as
+          //the score.
+          //distance = 
+          //  calcLevenshteinDistance(companyName,companyNameInError);
+
+          score=calcLengthOfMatchingWords(companyNameInError,companyName);
+
+          if(companyName.compare("Jasa Marga Tbk")==0 
+            && companyNameInError.compare("JASA MARGA")==0){
+              bool here=true;
+            }
+
+          unsigned int minScoreB = std::round(companyName.length()*0.5); //Just to filter out Tbk, GmbH, etc
+          if(minScoreB < 5){
+            minScoreB = 5;
+          }
+
+          if(score > bestScore && score > minScoreA && score > minScoreB ){
+            bestScore=score;
+            candidateFound=true;
+            FinancialAnalysisToolkit::
+              getJsonString(itSym["Name"],bestName);            
+            FinancialAnalysisToolkit::
+              getJsonString(itSym["Code"],bestCode);
+          }
+        }
+
+        if(candidateFound==true){
             std::string ex;
-            FinancialAnalysisToolkit::getJsonString(itSym["Code"],eodCode);
-            eodCode.append(".");
-            eodCode.append(itExc);
+            
+            bestCode.append(".");
+            bestCode.append(itExc);
             patchResults[tickerName]["PatchFound"] = true;
-            patchResults[tickerName]["PatchPrimaryTicker"] = eodCode;
-            patchResults[tickerName]["PatchName"]=companyName;
-            patchResults[tickerName]["PatchFitScore"] = 1.0;
+            patchResults[tickerName]["PatchPrimaryTicker"] = bestCode;
+            patchResults[tickerName]["PatchName"]=bestName;
+            patchResults[tickerName]["PatchSimilarityScore"] = bestScore;
 
             itPatchData.patchFound[indexTicker]=true;
 
-            if(verbose){
-              ++patchCount;
-              std::string code;
-              FinancialAnalysisToolkit::
-                getJsonString(scanResults[tickerName]["Code"],code);                
-              std::cout   << patchCount 
-                  << ".  " << code << " " << companyNameInError << std::endl  
-                  << "    " << "PrimaryTickerPatch: " 
-                  << companyName << " (" << eodCode << ")" << std::endl;
-            }
-
-            break;
-          }
         }
       }
 
@@ -279,6 +413,27 @@ int main (int argc, char* argv[]) {
   //Report on the items that were patched and not.
   if(verbose){
     std::cout << std::endl;
+    
+    std::cout << "Patches found for : " << std::endl;
+    unsigned int  countPatches = 0;
+    for(auto& itData : primaryTickerPatchDataVector){
+      for(unsigned int i=0; i<itData.patchFound.size();++i){
+        if(itData.patchFound[i]==true){
+          std::string companyNameInError("");
+          FinancialAnalysisToolkit::
+            getJsonString(scanResults[itData.tickersInError[i]]["Name"],
+                          companyNameInError);
+          ++countPatches;
+          std::cout << countPatches 
+              << ".  " << itData.tickersInError[i] << std::endl
+              << "  " << companyNameInError << std::endl
+              << "  " << patchResults[itData.tickersInError[i]]
+                                     ["PatchName"].get<std::string>() 
+              << std::endl;
+        }
+      }
+    }
+    std::cout << std::endl << std::endl;
     std::cout << "Patches could not be found for : " << std::endl;
     unsigned int  countMissingPatches = 0;
     for(auto& itData : primaryTickerPatchDataVector){
@@ -290,9 +445,9 @@ int main (int argc, char* argv[]) {
                           companyNameInError);
           ++countMissingPatches;
           std::cout << countMissingPatches 
-              << ".  " << itData.tickersInError[i] 
-              << " (" << companyNameInError << ")" << std::endl;
-          std::cout << "     " << itData.currency << ": ";
+              << ".  " << itData.tickersInError[i] << std::endl
+              << "  " << companyNameInError  << std::endl;
+          std::cout << "  " << itData.currency << ": ";
           for(unsigned int j=0; j<itData.exchanges.size();++j){
             std::cout << itData.exchanges[j] << " " ;
           }
