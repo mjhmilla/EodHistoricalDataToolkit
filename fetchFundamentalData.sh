4134cf51ff6242c5e6c34c3bf82507100f915947
mjhmilla <millard.matthew@gmail.com>
2023-12-31 11:05:53 +0100

    updated the fetch scripts to include the new field (-d) that specifies the fundamental data folder

--- fetchFundamentalData.sh
+++ fetchFundamentalData.sh
@@ -11,6 +11,6 @@ EX="$1"
 
 
 cd build
-./fetch -f ${EOD_TOOLKIT_HOME}/data/"$EX"/fundamentalData/ -u ${EOD_FUNDAMENTAL_DATA} -d ${EOD_FUNDAMENTAL_DATA} -k ${EOD_API_TOKEN} -t ${EOD_TOOLKIT_HOME}/data/"$EX".json -x "$EX" -g -v | tee ${EOD_TOOLKIT_HOME}/data/"$EX"/fundamentalData."$EX".log
+./fetch -f ${EOD_TOOLKIT_HOME}/data/"$EX"/fundamentalData/ -u ${EOD_FUNDAMENTAL_DATA} -d ${EOD_TOOLKIT_HOME}/data/"$EX"/fundamentalData/ -k ${EOD_API_TOKEN} -t ${EOD_TOOLKIT_HOME}/data/"$EX".json -x "$EX" -g -v | tee ${EOD_TOOLKIT_HOME}/data/"$EX"/fundamentalData."$EX".log
 cd ..
 
--- fetchHistoricalData.sh
+++ fetchHistoricalData.sh
@@ -4,11 +4,10 @@ EX="$1"
 
 rm -r ${EOD_TOOLKIT_HOME}/data/"$EX"/historicalData/
 
-mkdir ${EOD_TOOLKIT_HOME}/data/"$EX"
 mkdir ${EOD_TOOLKIT_HOME}/data/"$EX"/historicalData/
 
 
 cd build
-./fetch -f ${EOD_TOOLKIT_HOME}/data/"$EX"/historicalData/ -u ${EOD_HISTORICAL_DATA} -d ${EOD_FUNDAMENTAL_DATA} -x "$EX" -k ${EOD_API_TOKEN} -t ${EOD_TOOLKIT_HOME}/data/"$EX"/"$EX".json -g -v | tee ${EOD_TOOLKIT_HOME}/data/"$EX"/historicalData."$EX".log
+./fetch -f ${EOD_TOOLKIT_HOME}/data/"$EX"/historicalData/ -u ${EOD_HISTORICAL_DATA} -d ${EOD_TOOLKIT_HOME}/data/"$EX"/fundamentalData/ -x "$EX" -k ${EOD_API_TOKEN} -t ${EOD_TOOLKIT_HOME}/data/"$EX".json -g -v | tee ${EOD_TOOLKIT_HOME}/data/"$EX"/historicalData."$EX".log
 cd ..
 
