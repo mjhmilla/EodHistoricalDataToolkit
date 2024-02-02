9dd975107c5a7ce28e316555a60ddf61907a84dc
mjhmilla <millard.matthew@gmail.com>
2024-02-02 18:24:30 +0100

    added new tag to calculate.sh (-u for the mature fraction of capitalization from debt)

--- calculate.sh
+++ calculate.sh
@@ -3,5 +3,5 @@
 EX="$1"
 
 cd build
-./calculate -f ${EOD_TOOLKIT_HOME}/data/"$EX"/fundamentalData/ -p ${EOD_TOOLKIT_HOME}/data/"$EX"/historicalData/ -x STU -d ${EOD_TOOLKIT_HOME}/data/defaultSpreadTable.json -y ${EOD_TOOLKIT_HOME}/data/bondYieldTable.json -c 2.5 -t 0.30 -r 0.025 -e 0.05 -b 1.0 -n 3 -m 5 -a 35 -l -o ${EOD_TOOLKIT_HOME}/data/"$EX"/analysisData/ -v
+./calculate -f ${EOD_TOOLKIT_HOME}/data/"$EX"/fundamentalData/ -p ${EOD_TOOLKIT_HOME}/data/"$EX"/historicalData/ -x STU -d ${EOD_TOOLKIT_HOME}/data/defaultSpreadTable.json -y ${EOD_TOOLKIT_HOME}/data/bondYieldTable.json -c 2.5 -t 0.40 -r 0.025 -e 0.05 -b 1.0 -u 0.2 -n 3 -m 5 -a 35 -l -o ${EOD_TOOLKIT_HOME}/data/"$EX"/analysisData/ -v
 cd ..
