6b43ac8d64d1950433c906ce23677627e4f38faf
mjhmilla <millard.matthew@gmail.com>
2024-12-15 09:53:12 +0100

    completed writing a function to apply a user-specified filter (configured in a json file) that can apply to any piece of data in a fundamental, historical, or calculated file.

--- src/generateMarketReports.cc
+++ src/generateMarketReports.cc
@@ -18,10 +18,182 @@ bool applyFilter(std::string &tickerFileName,
                  std::string &fundamentalFolder,
                  std::string &historicalFolder,
                  std::string &calculateDataFolder,
-                 nlohmann::ordered_json &marketReportConfig){
-  bool valid = true;
+                 nlohmann::ordered_json &marketReportConfig,
+                 bool replaceNansWithMissingData,
+                 bool verbose){
 
-  return valid;
+  bool valueFilter = true;
+
+
+
+  bool filterFieldExists = marketReportConfig.contains("filter");
+
+  if(filterFieldExists){
+    for( auto const &el : marketReportConfig["filter"].items() ){
+      //
+      // Retreive the filter data
+      //
+      std::string filterName(el.key());
+
+      std::string folder; 
+      JsonFunctions::getJsonString(el.value()["folder"],folder);
+
+      std::vector< std::string > fieldVector;
+      std::string fieldName("");
+      for( auto const &fieldEntry : el.value()["field"]){
+        JsonFunctions::getJsonString(fieldEntry,fieldName);
+        fieldVector.push_back(fieldName);
+      }
+
+      std::string conditionName("");
+      JsonFunctions::getJsonString(el.value()["condition"],conditionName);
+
+      std::string operatorName("");
+      JsonFunctions::getJsonString(el.value()["operator"],operatorName);
+
+      std::string valueType("");
+      JsonFunctions::getJsonString(el.value()["valueType"],valueType);
+
+
+      std::vector< std::string > valueStringVector;
+      std::string valueStringName;
+      if(valueType == "string"){
+        for( auto const &fieldEntry : el.value()["values"]){
+          JsonFunctions::getJsonString(fieldEntry,valueStringName);
+          valueStringVector.push_back(valueStringName);
+        }  
+      }
+
+      std::vector< double > valueFloatVector;
+      double valueFloatName;
+      if(valueType == "float"){
+        for( auto const &fieldEntry : el.value()["values"]){
+          valueFloatName = JsonFunctions::getJsonFloat(fieldEntry);
+          valueFloatVector.push_back(valueFloatName);
+        }  
+      }
+
+      //
+      // Fetch the target data and apply the filter
+      //
+      std::string fullPathFileName("");      
+      if(folder == "fundamentalData"){
+        fullPathFileName = fundamentalFolder;                
+      }else if(folder == "historicalData"){
+        fullPathFileName = historicalFolder;                        
+      }else if(folder == "calculateData"){
+        fullPathFileName = calculateDataFolder;                      
+      }else{
+        std::cerr << "Error: in filter " << filterName 
+                  << " the folder field is " << folder 
+                  << " but should be fundamentalData, historicalData,"
+                  << " or calculateData" 
+                  << std::endl;
+        std::abort();                  
+      }
+
+      fullPathFileName.append(tickerFileName);
+
+      nlohmann::ordered_json targetJsonTable;
+      bool loadedConfiguration = 
+        JsonFunctions::loadJsonFile(fullPathFileName,
+                                    targetJsonTable,
+                                    verbose); 
+
+      std::string valueString;
+      double valueFloat;
+      std::vector < bool > valueBoolVector;
+
+      if(valueType == "string"){
+        JsonFunctions::getJsonString(targetJsonTable, fieldVector, valueString);
+
+        //Evaluate all of the individual values against the target
+        for(size_t i = 0; i <  valueStringVector.size(); ++i){
+          if(conditionName == "=="){
+            bool equalityTest = (valueString == valueStringVector[i]);
+            valueBoolVector.push_back(equalityTest);
+          }else if (conditionName == "!="){
+            bool equalityTest = (valueString != valueStringVector[i]);
+            valueBoolVector.push_back(equalityTest);
+          }else{
+            std::cerr << "Error: in filter " << filterName 
+                  << " the condition field should be " 
+                  << " == or != for a string"
+                  << " but is instead " << conditionName  
+                  << std::endl;
+            std::abort();   
+          }        
+        }
+
+      }else if(valueType == "float"){
+        valueFloat = JsonFunctions::getJsonFloat(targetJsonTable, fieldVector,
+                                                 replaceNansWithMissingData);
+
+        //Evaluate all of the individual values against the target
+        for(size_t i = 0; i <  valueFloatVector.size(); ++i){
+          if(conditionName == "=="){
+            bool equalityTest = (valueFloat == valueFloatVector[i]);
+            valueBoolVector.push_back(equalityTest);
+          }else if (conditionName == "!="){
+            bool equalityTest = (valueFloat != valueFloatVector[i]);
+            valueBoolVector.push_back(equalityTest);
+          }else if (conditionName == ">"){
+            bool equalityTest = (valueFloat > valueFloatVector[i]);
+            valueBoolVector.push_back(equalityTest);
+          }else if (conditionName == "<"){
+            bool equalityTest = (valueFloat < valueFloatVector[i]);
+            valueBoolVector.push_back(equalityTest);
+          }else if (conditionName == ">="){
+            bool equalityTest = (valueFloat >= valueFloatVector[i]);
+            valueBoolVector.push_back(equalityTest);
+          }else if (conditionName == "<="){
+            bool equalityTest = (valueFloat <= valueFloatVector[i]);
+            valueBoolVector.push_back(equalityTest);
+          }else{
+            std::cerr << "Error: in filter " << filterName 
+                  << " the condition field should be one of" 
+                  << " (==, !=, >, <, >=, <=) for a float"
+                  << " but is instead " << conditionName  
+                  << std::endl;
+            std::abort();   
+          }        
+        }
+
+
+      }else{
+        std::cerr << "Error: in filter " << filterName 
+                  << " valueType should be string or float but is instead" 
+                  << valueType 
+                  << std::endl;        
+      }
+
+
+      //Apply the operator between all of the values to yield the final 
+      //filter value
+      valueFilter = valueBoolVector[0];
+      for(size_t i = 1; i < valueBoolVector.size(); ++i){
+        if(operatorName == "||"){
+          valueFilter = (valueFilter || valueBoolVector[i]);
+        }else if( operatorName == "&&"){
+          valueFilter = (valueFilter && valueBoolVector[i]);
+        }else{
+          std::cerr << "Error: in filter " << filterName 
+                << " the operator field should be " 
+                << " || or && for a string"
+                << " but is instead " << operatorName  
+                << std::endl;
+          std::abort();             
+        }
+      }
+
+
+
+    }
+
+
+  }
+
+  return valueFilter;
 };
 
 
@@ -154,56 +326,100 @@ int main (int argc, char* argv[]) {
                                 verbose);
                                   
 
-  std::vector< std::string > processedTickers;
+  if(!loadedConfiguration){
+    std::cerr << "Error: cannot open " << marketReportConfigurationFilePath 
+              << std::endl;
+    
+  }
 
-  int totalFileCount = 0;
-  int validFileCount = 0;
-  std::string analysisExt = ".json";  
+
+
+  if(loadedConfiguration){
+    std::vector< std::string > processedTickers;
+
+    int totalFileCount = 0;
+    int validFileCount = 0;
+    std::string analysisExt = ".json";  
 
 
 
-  //Go through every ticker file in the calculate folder:
-  //1. Create a ticker folder in the reporting folder
-  //2. Save to this ticker folder:
-  //  a. The pdf of the ticker's plots
-  //  b. The LaTeX file for the report (as an input)
-  //  c. A wrapper LaTeX file to generate an individual report
+    //Go through every ticker file in the calculate folder:
+    //1. Create a ticker folder in the reporting folder
+    //2. Save to this ticker folder:
+    //  a. The pdf of the ticker's plots
+    //  b. The LaTeX file for the report (as an input)
+    //  c. A wrapper LaTeX file to generate an individual report
 
-  for (const auto & file 
-        : std::filesystem::directory_iterator(calculateDataFolder)){  
+    std::vector< std::string > filteredTickers;
 
-    ++totalFileCount;
-    bool validInput = true;
+    for (const auto & file 
+          : std::filesystem::directory_iterator(calculateDataFolder)){  
 
-    std::string fileName   = file.path().filename();
+      ++totalFileCount;
+      bool validInput = true;
 
-    std::size_t fileExtPos = fileName.find(analysisExt);
+      std::string fileName   = file.path().filename();
+
+      std::size_t fileExtPos = fileName.find(analysisExt);
+
+      if(verbose){
+        std::cout << totalFileCount << '\t' << fileName << std::endl;
+      }
+      
+      if( fileExtPos == std::string::npos ){
+        validInput=false;
+        if(verbose){
+          std::cout << '\t' 
+                    << "Skipping " << fileName 
+                    << " should end in .json but this does not:" << std::endl; 
+        }
+      }
+
+      //Check to see if this ticker passes the filter
+      bool tickerPassesFilter=false;
+      if(validInput){
+        tickerPassesFilter = applyFilter( fileName,  
+                                  fundamentalFolder,
+                                  historicalFolder,
+                                  calculateDataFolder,
+                                  marketReportConfig,
+                                  replaceNansWithMissingData,
+                                  verbose);
+      }    
+
+      if(tickerPassesFilter){
+        filteredTickers.push_back(fileName);
+      }
+      if(verbose){
+        if(tickerPassesFilter){
+          std::cout << '\t'
+                    << "passed"
+                    << std::endl;
+        }else{
+          std::cout << '\t'
+                    << "filtered out"
+                    << std::endl;
+        }
+      }
+
+    }
 
     if(verbose){
-      std::cout << totalFileCount << '\t' << fileName << std::endl;
+      std::cout << '\n' << '\n'
+                << filteredTickers.size() << '\t' 
+                << "files passed the filter"
+                << '\n' 
+                << totalFileCount - filteredTickers.size() << '\t' 
+                << "files removed"
+                << std::endl;
+
+      std::cout << '\n' << '\n'
+                << "Tickers that passed the filter: " << std::endl;
+      for(size_t i=0; i< filteredTickers.size(); ++i){
+        std::cout << i << '\t' << filteredTickers[i] << std::endl;
+      }                
+
     }
-    
-    if( fileExtPos == std::string::npos ){
-      validInput=false;
-      if(verbose){
-        std::cout << '\t' 
-                  << "Skipping " << fileName 
-                  << " should end in .json but this does not:" << std::endl; 
-      }
-    }
-
-    //Check to see if this ticker passes the filter
-    if(validInput){
-      validInput = applyFilter( fileName,  
-                                fundamentalFolder,
-                                historicalFolder,
-                                calculateDataFolder,
-                                marketReportConfig);
-    }    
-
-    
-
-
   }
 
   return 0;
